373823322148f4cf8a230f6fcf9bfa9bed41a440
gdany91 <you@example.comdgarofalo52@gmail.com>
2023-02-03 09:14:08 +0100

    first commit

---
+++ database/migrations/2023_02_03_075252_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("agency", 32);
+            $table->string("code", 10)->unique();
+            $table->date("departure");
+            $table->date("arrival");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ database/migrations/2023_02_03_075640_create_travel_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id(); 
+            $table->string("place");
+            $table->smallinteger("price") -> unsigned();          
+            $table->string("code", 10) -> nullable(false) -> unique();
+            $table->text("description");
+            $table->date("departure_date");
+            $table->date("returned_date");
+            $table->tinyInteger("vote");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+};
